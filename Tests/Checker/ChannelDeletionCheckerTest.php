32eeec21077ca746405382b7b833b3332700210a
Mateusz <mateusz@rustle.pl>
2025-04-23 14:55:51 +0200

    Add PHPUnit tests

---
+++ Tests/Checker/ChannelDeletionCheckerTest.php
@@ -0,0 +1,86 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Sylius Sp. z o.o.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Sylius\Component\Channel\Checker\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Sylius\Component\Channel\Checker\ChannelDeletionChecker;
+use Sylius\Component\Channel\Model\ChannelInterface;
+use Sylius\Component\Channel\Repository\ChannelRepositoryInterface;
+
+final class ChannelDeletionCheckerTest extends TestCase
+{
+    private ChannelRepositoryInterface $channelRepository;
+
+    private ChannelDeletionChecker $checker;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->channelRepository = $this->createMock(ChannelRepositoryInterface::class);
+        $this->checker = new ChannelDeletionChecker($this->channelRepository);
+    }
+
+    public function testShouldReturnsTrueWhenChannelIsDisabled(): void
+    {
+        $channel = $this->createMock(ChannelInterface::class);
+        $channel->expects($this->once())
+            ->method('isEnabled')
+            ->willReturn(false);
+
+        self::assertTrue($this->checker->isDeletable($channel));
+    }
+
+    public function testShouldReturnsTrueWhenAtLeastTwoChannelsAreEnabled(): void
+    {
+        $channel = $this->createMock(ChannelInterface::class);
+        $anotherChannel = $this->createMock(ChannelInterface::class);
+
+        $channel->expects($this->once())
+            ->method('isEnabled')
+            ->willReturn(true);
+        $this->channelRepository->expects($this->once())
+            ->method('findEnabled')
+            ->willReturn([$channel, $anotherChannel]);
+
+        self::assertTrue($this->checker->isDeletable($channel));
+    }
+
+    public function testShouldReturnsFalseWhenOnlyOneChannelIsEnabled(): void
+    {
+        $channel = $this->createMock(ChannelInterface::class);
+
+        $channel->expects($this->once())
+            ->method('isEnabled')
+            ->willReturn(true);
+        $this->channelRepository->expects($this->once())
+            ->method('findEnabled')
+            ->willReturn([$channel]);
+
+        self::assertFalse($this->checker->isDeletable($channel));
+    }
+
+    public function testShouldReturnsFalseWhenNoChannelIsEnabled(): void
+    {
+        $channel = $this->createMock(ChannelInterface::class);
+
+        $channel->expects($this->once())
+            ->method('isEnabled')
+            ->willReturn(true);
+        $this->channelRepository->expects($this->once())
+            ->method('findEnabled')
+            ->willReturn([]);
+
+        self::assertFalse($this->checker->isDeletable($channel));
+    }
+}
---
+++ Tests/Factory/ChannelFactoryTest.php
@@ -0,0 +1,63 @@
+<?php
+
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Sylius Sp. z o.o.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace Tests\Sylius\Component\Channel\Factory;
+
+use PHPUnit\Framework\TestCase;
+use Sylius\Component\Channel\Factory\ChannelFactory;
+use Sylius\Component\Channel\Factory\ChannelFactoryInterface;
+use Sylius\Component\Channel\Model\ChannelInterface;
+use Sylius\Resource\Factory\FactoryInterface;
+
+class ChannelFactoryTest extends TestCase
+{
+    public function testShouldImplementsChannelFactoryInterface(): void
+    {
+        $defaultFactory = $this->createMock(FactoryInterface::class);
+        $factory = new ChannelFactory($defaultFactory);
+
+        self::assertInstanceOf(ChannelFactoryInterface::class, $factory);
+    }
+
+    public function testShouoldCreatesChannelWithName(): void
+    {
+        $defaultFactory = $this->createMock(FactoryInterface::class);
+        $channel = $this->createMock(ChannelInterface::class);
+
+        $defaultFactory->expects($this->once())
+            ->method('createNew')
+            ->willReturn($channel);
+
+        $channel->expects($this->once())
+            ->method('setName')
+            ->with('United States Webstore');
+
+        $factory = new ChannelFactory($defaultFactory);
+
+        self::assertSame($channel, $factory->createNamed('United States Webstore'));
+    }
+
+    public function testShouldCreatesEmptyChannel(): void
+    {
+        $defaultFactory = $this->createMock(FactoryInterface::class);
+        $channel = $this->createMock(ChannelInterface::class);
+
+        $defaultFactory->expects($this->once())
+            ->method('createNew')
+            ->willReturn($channel);
+
+        $factory = new ChannelFactory($defaultFactory);
+
+        self::assertSame($channel, $factory->createNew());
+    }
+}
